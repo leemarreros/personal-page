eb747b75da97b387a1004e8591e0cb402f10d70a
Lee Crosby <lee.marreros@pucp.pe>
2015-12-04 14:45:17 -0800

    Implements resources section

--- client/components/Menu/Resources.js
+++ client/components/Menu/Resources.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import resources from '../../resources'
 
 class Resources extends React.Component{
   constructor(props) {
@@ -12,7 +13,24 @@ class Resources extends React.Component{
   render() {
     return (
       <div className='wrapperResources'>
-        Resources
+        {resources.map((resource, i)=>{
+          return (
+            <div className="eachSource" key={i}>
+              <div><h1>{resource.name}</h1></div>
+              <div className='wrapperLinks'>
+                {resource.sources.map((link, i)=>{
+                  return (
+                    <span key={i}>
+                      <a href={link} target="_blank">
+                        <h1>{link}</h1>
+                      </a>
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
       );
   }
---
+++ client/resources.js
@@ -0,0 +1,275 @@
+var resources =
+[
+  {
+    name: 'NODEJS STREAMS',
+    sources: [
+      'https://github.com/substack/stream-handbook'
+    ]
+  },
+  {
+    name: 'MONGODB, REACT, SOCKET IO - FULL STACK',
+    sources: [
+      'http://sahatyalkabov.com/create-a-character-voting-app-using-react-nodejs-mongodb-and-socketio/'
+    ]
+  },
+  {
+    name: 'CSS icons, effects, loaders',
+    sources: [
+      'http://callmenick.com/categories/resources'
+    ]
+  },
+  {
+    name: 'DOCKER, REACT',
+    sources: [
+      'https://medium.com/@tribou/react-and-flux-a-docker-development-workflow-469957f3bbf0#.af32hdpfo'
+    ]
+  },
+  {
+    name: 'NODEJS Encoding and decoding',
+    sources: [
+      'http://www.hacksparrow.com/base64-encoding-decoding-in-node-js.html'
+    ]
+  },
+  {
+    name: 'FLEXBOX',
+    sources: [
+      'http://flexbox.io/#/view/TrsrNZo0pOY',
+      'http://jonibologna.com/flexbox-cheatsheet/',
+      'https://www.youtube.com/watch?v=_98SE8WUvLk'
+    ]
+  },
+  {
+    name: 'REACTIVE PROGRAMMING',
+    sources: [
+      'https://github.com/substack/stream-handbook'
+    ]
+  },
+  {
+    name: 'REACTIVE PROGRAMMING',
+    sources: [
+      'https://gist.github.com/dmvaldman/f957dd9a8ed3f6edf35d',
+      'https://egghead.io/series/introduction-to-reactive-programming',
+      'https://gist.github.com/staltz/868e7e9bc2a7b8c1f754'
+    ]
+  },
+  {
+    name: 'VIRTUAL HOSTS - APACHE',
+    sources: [
+      'http://coolestguidesontheplanet.com/set-virtual-hosts-apache-mac-osx-10-10-yosemite/'
+    ]
+  },
+  {
+    name: 'STITCH',
+    sources: [
+      'https://github.com/mattsjohnston/stitch'
+    ]
+  },
+  {
+    name: 'REACTIVE PROGRAMMING',
+    sources: [
+      'https://github.com/substack/stream-handbook'
+    ]
+  },
+  {
+    name: 'REACT router',
+    sources: [
+      'https://github.com/rackt/react-router',
+      'https://medium.com/the-exponent-log/routing-and-navigation-in-react-native-6b27bee39603'
+    ]
+  },
+  {
+    name: 'REACTIVE PROGRAMMING',
+    sources: [
+      'https://github.com/substack/stream-handbook'
+    ]
+  },
+  {
+    name: 'dropdown menu',
+    sources: [
+      'https://www.codementor.io/reactjs/tutorial/create-a-dropdown-using-react-js--font-awesome-and-less-css'
+    ]
+  },
+  {
+    name: 'REACT animation',
+    sources: [
+      'http://jsfiddle.net/pchng/17rq3s6d/1/light/',
+      'http://unitstep.net/blog/2015/03/03/using-react-animations-to-transition-between-ui-states/'
+    ]
+  },
+  {
+    name: 'UX',
+    sources: [
+      'https://www.mysliderule.com/learning-paths/user-experience-design/learn/?'
+    ]
+  },
+  {
+    name: 'REDUX',
+    sources: [
+      'http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html'
+    ]
+  },
+  {
+    name: 'GRAPHQL',
+    sources: [
+      'https://learngraphql.com/basics/lets-learn-graphql',
+      'https://learngraphql.com/basics/more-on-graphql',
+      'https://github.com/chentsulin/awesome-graphql#lib-js'
+    ]
+  },
+  {
+    name: 'Reactive programming (RxJS)',
+    sources: [
+      'https://github.com/Reactive-Extensions/RxJS'
+    ]
+  },
+  {
+    name: 'ALIAS IN BASH',
+    sources: [
+      'http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_03_05.html'
+    ]
+  },
+  {
+    name: 'GITHUB SHORTCUTS CONFIG',
+    sources: [
+      'https://gist.github.com/davemo/5329141#file-gitconfig'
+    ]
+  },
+  {
+    name: 'COFFEE SCRIPT',
+    sources: [
+      'http://tbranyen.com/post/coffeescript-has-the-ideal-syntax-for-configurations'
+    ]
+  },
+  {
+    name: 'RESPONSIVE IMAGES',
+    sources: [
+      'http://codepen.io/Tigt/post/when-responsive-images-get-ugly',
+      'https://html.spec.whatwg.org/multipage/embedded-content.html#the-picture-element',
+      'https://www.filamentgroup.com/lab/to-picturefill.html',
+      'https://responsivedesign.is/resources/images/picture-fill',
+      'https://scottjehl.github.io/picturefill/',
+      'https://github.com/nwtn/grunt-respimg',
+      'https://github.com/smaxtastic/grunt-responsive-images-extender creates the HTML responsive image for you',
+      'https://imageoptim.com/',
+      'https://github.com/toy/image_optim'
+    ]
+  },
+  {
+    name: 'Human design',
+    sources: [
+      'http://www.fastcodesign.com/3053406/how-apple-is-giving-design-a-bad-name?utm_source',
+      'http://www.nngroup.com/people/bruce-tognazzini/'
+    ]
+  },
+  {
+    name: 'Promises',
+    sources: [
+      'http://www.datchley.name/es6-promises/'
+    ]
+  },
+  {
+    name: 'Login facebook',
+    sources: [
+      'https://gist.github.com/jforaker/bbd1b27138f259f2d7fa#add-a-nativemodules-js-module-to-your-project-facebookloginmoduleandroidjs-below'
+    ]
+  },
+  {
+    name: 'website cost in terms of download',
+    sources: [
+      'https://whatdoesmysitecost.com/'
+    ]
+  },
+  {
+    name: 'Deferred image loading: lazy approach',
+    sources: [
+      'https://github.com/aFarkas/lazysizes'
+    ]
+  },
+  {
+    name: 'Color blind',
+    sources: [
+      'http://colororacle.org/'
+    ]
+  },
+  {
+    name: 'Text accessibility',
+    sources: [
+      'http://www.w3.org/TR/WCAG20-TECHS/ARIA1.html'
+    ]
+  },
+  {
+    name: 'text alternative for non-text content',
+    sources: [
+      'http://www.w3.org/WAI/GL/wiki/Using_aria-labelledby_to_provide_a_text_alternative_for_non-text_content'
+    ]
+  },
+  {
+    name: 'longdesc implementation report',
+    sources: [
+      'http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html'
+    ]
+  },
+  {
+    name: 'SVG accessibility using aria',
+    sources: [
+      'https://www.paciellogroup.com/blog/2013/12/using-aria-enhance-svg-accessibility/'
+    ]
+  },
+  {
+    name: 'Mobile interactions of the week #1',
+    sources: [
+      'https://medium.com/@usemuzli/mobile-interactions-of-the-week-1-5103613c6aad'
+    ]
+  },
+  {
+    name: 'CSS',
+    sources: [
+      'http://rachelnabors.com/css-animation-master-class/#slide-11'
+    ]
+  },
+  {
+    name: 'PRODUCT DESIGN',
+    sources: [
+      'https://www.udacity.com/course/product-design--ud509'
+    ]
+  },
+  {
+    name: 'mongoose and react',
+    sources: [
+      'https://scotch.io/tutorials/build-a-real-time-twitter-stream-with-node-and-react-js'
+    ]
+  },
+  {
+    name: 'Learn framer',
+    sources: [
+      'http://www.stakelon.com/framerworkshop/index.html#/3',
+      'http://examples.framerjs.com/#video-basic.framer',
+      'http://framerjs.com/examples/',
+      'http://www.stakelon.com/2014/04/hello-framer-getting-started-with-framer-js/',
+      'http://thatedchao.com/published/2014/09/27/framer.html',
+      'http://www.darrinhenein.com/rapid-prototyping-part-one/',
+      'https://github.com/koenbok/Framer'
+    ]
+  },
+  {
+    name: 'READING FILES IN JS',
+    sources: [
+      'http://www.html5rocks.com/en/tutorials/file/dndfiles/'
+    ]
+  },
+  {
+    name: 'REACTIVE: CONFERENCE 2015',
+    sources: [
+      'https://www.youtube.com/watch?v=BfzjuhX4wJ0&feature=youtu.be&t=11m53s'
+    ]
+  },
+  {
+    name: 'MONGODB and node.js',
+    sources: [
+      'http://blog.modulus.io/mongodb-tutorial'
+    ]
+  },
+
+]
+
+module.exports = resources;
